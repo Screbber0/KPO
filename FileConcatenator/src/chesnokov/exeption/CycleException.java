9ef750815e548231ac2d8cc103ebb0631b456604
Screbber0 <116518639+Screbber0@users.noreply.github.com>
2022-12-28 11:15:11 +0300

    Программа для конкатенации файлов в один
    Программа конкатенирует файлы в один по указанным require

---
+++ FileConcatenator/src/chesnokov/exeption/CycleException.java
@@ -0,0 +1,10 @@
+package chesnokov.exeption;
+
+/**
+ * Класс для исключений вызываемых из-за зацикленности графа
+ */
+public class CycleException extends Exception {
+    public CycleException(String message) {
+        super(message);
+    }
+}
